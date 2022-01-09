b24842cd04cc4f103a3ff702857c47704d0f1372
Engku Azrul <engkuazrull@gmail.com>
2022-01-09 23:18:04 +0800

    create button, pagelayout, header, home, login component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        background: white;
+        min-height: 100vh;
+        margin: 0;
+        color: black;
+        font-family: 'Kaushan Script';
+    }
+`;
 
 const App = () => {
     return (
-        <div>
-            App
-        </div>
+        <>
+            <GlobalStyle />
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/login'>
+                        <Login />
+                    </Route>
+                    <Route path='/'>
+                        <Home />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </>
     );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { PageLayout } from "../common";
+
+export default function Home() {
+    return (
+        <PageLayout>
+            <h1>
+                App
+            </h1>
+        </PageLayout>
+    )
+}
