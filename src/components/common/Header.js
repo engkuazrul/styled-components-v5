849dfb16a6bf4819ada6dac37a34cb3c42e423b3
Engku Azrul <engkuazrull@gmail.com>
2022-01-10 22:55:37 +0800

    create responsive navbar

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link as ReactRouterDomLink, useLocation } from "react-router-dom";
 
@@ -10,22 +10,35 @@ const HeaderWrapper = styled.header`
     padding: 0 16px;
     position: fixed;
     top: 0;
-    background: #eee;
+    background-image: linear-gradient(to right, #f8049c, #fdd54f);
+    border-bottom: 3px solid #fdd54f;
 `;
 
 const Menu = styled.nav`
-    display: flex;
-    position: relative;
-    width: initial;
-    border-bottom: none;
-    margin: auto 0 auto auto;
+    display: ${p => p.open ? 'block' : 'none'};
     font-family: 'Open Sans';
-    background: none;
-    left: initial;
-    top: initial;
+    position: absolute;
+    width: 100%;
+    top: 60px;
+    left: 0;
+    padding: 8px;
+    box-sizing: border-box;
+    border-bottom: 3px solid #fdd54f;
+    background: white;
+
+    @media(min-width: 768px){
+        display: flex;
+        background: none;
+        left: initial;
+        top: initial;
+        margin: auto 0 auto auto;
+        border-bottom: none;
+        position: relative;
+        width: initial;
+    }
 `;
 
-const Link = ({isActive, children, ...props}) => {
+const Link = ({ isActive, children, ...props }) => {
     return (
         <ReactRouterDomLink {...props}>
             {children}
@@ -40,14 +53,39 @@ const StyledLink = styled(Link)`
     box-sizing: border-box;
     margin: auto 0;
     font-weight: ${p => p.isActive ? 'bold' : 'normal'};
+    color: black;
+`;
+
+const MobileMenuIcon = styled.div`
+    margin: auto 0 auto auto;
+    width: 25px;
+    min-width: 25px;
+    padding: 5px;
+
+    >div {
+        height: 3px;
+        background: black;
+        margin: 5px 0;
+        width: 100%;
+    }
+
+    @media(min-width: 768px) {
+        display: none;
+    }
 `;
 
 export function Header() {
-    const {pathname} = useLocation();
+    const { pathname } = useLocation();
+    const[menuOpen, setMenuOpen] = useState(false);
 
     return (
         <HeaderWrapper>
-            <Menu>
+            <MobileMenuIcon onClick={() => setMenuOpen(s => !s)}>
+                <div />
+                <div />
+                <div />
+            </MobileMenuIcon>
+            <Menu open={menuOpen}>
                 <StyledLink to='/' isActive={pathname === '/'}>
                     Home
                 </StyledLink>
